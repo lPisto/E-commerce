acd3b76ea8e9ded26764d7acdc3d51916aabcfe2
Lucas <Pisto@gmail.com>
2022-05-19 21:20:13 -0300

    implementing ejs

--- routes/routes.js
+++ routes/routes.js
@@ -3,35 +3,56 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/index.html"));
+  res.render("index");
 });
 
 router.get("/index.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
-  });
+  res.render("index");
+});
 
 router.get("/products.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/products.html"));
+  res.render("products");
 });
 
 router.get("/login.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/login.html"));
+  res.render("login");
 });
 
-router.get("/signUp.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/signUp.html"));
+router.get("/signUp", (req, res) => {
+  res.render("signUp");
 });
+
+// router.post('/signUp.html', (req, res) => {
+//   const {name, surname, email, password, confirm_password, contact} = req.body;
+//   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+  
+//   if(!regexEmail.test(email.value)){
+//     warning.innerHTML = `El email no es valido <br>`
+//   }
+
+//   if (password != confirm_password) {
+//     alert('Password do not match');    
+//   }
+//   if (password.length < 4) {
+//     errors.push({text: 'Password must be at least 4 characters'})
+//   }
+//   if (errors.length > 0) {
+//     res.render('signUp.html', {errors, name, surname, email, password, confirm_password, contact});
+//   } else {
+//     res.send('ok')
+//   }
+// });
 
 router.get("/cart.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/cart.html"));
+  res.render("cart");
 });
 
 router.get("/success.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/success.html"));
+  res.render("success");
 });
 
 router.get("/settings.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/settings.html"));
+  res.render("settings");
 });
 
 
--- servidor.js
+++ servidor.js
@@ -7,9 +7,12 @@ const app = express();
 
 // settings
 app.set("port", process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
 
 // global variables
 
